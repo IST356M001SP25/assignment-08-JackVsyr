2974d9a1e9931d5a24c9596f3509f7d9398defad
JackVsyr <jgveneru@syr.edu>
2025-04-20 22:22:15 -0400

    map dashboard

--- code/map_dashboard.py
+++ code/map_dashboard.py
@@ -12,3 +12,17 @@ CUSE = (43.0481, -76.1474)  # center of map
 ZOOM = 14                   # zoom level
 VMIN = 1000                 # min value for color scale
 VMAX = 5000                 # max value for color scale
+
+map = pd.read_csv('/Users/jack/Downloads/IST 356/assignment-08-JackVsyr/cache/top_locations_mappable.csv')
+st.title('Top Locations for Parking Tickets in Syracuse')
+st.caption('This dashboard shows the tickets that were issued in the top locations with $1,000 or more in total violation amounts.')
+
+geo = gpd.GeoDataFrame(map, geometry=gpd.points_from_xy(map.lon, map.lat))
+map2 = folium.Map(location=CUSE, zoom_start=ZOOM)
+syr_map = geo.explore(geo['amount'], m=map2, 
+                       cmap="magma",vmin=VMIN, vmax=VMAX, 
+                       legend=True, legend_name='Amount',
+                       marker_type = "circle",
+                       marker_kwds = {"radius": 10, "fill": True},
+)
+sf.folium_static(syr_map, width=800, height=600)
